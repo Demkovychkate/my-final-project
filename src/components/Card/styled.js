1a24d6be6f8243728cc4320ff6311d3dbd1dd92b
Demkovychkate <Panchevakate1804@gmail.com>
2024-02-16 23:32:19 +0200

    Add Sparklines to the Header component

--- src/components/Card/styled.js
+++ src/components/Card/styled.js
@@ -1,12 +1,20 @@
-import { Card, Typography } from '@mui/material';
 import styled from 'styled-components';
 
-export const CardWrapper = styled(Card)`
+export const CardWrapper = styled.div`
+  
   width: 160px;
+  margin-top: 1.5%;
   `;
 
-export const DescriptionTypography = styled(Typography)`
-max-height: 100px;
+export const DescriptionTypography = styled.div`
+max-height: 70px;
 overflow: hidden;
 text-overflow: ellipsis;
+font-family: "Sacramento", cursive;
+color:white;
+display: flex;
+align-items: center;
+justify-content: center;
+font-size: 40px;
+
 `;
