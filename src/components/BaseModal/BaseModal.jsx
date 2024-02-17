7bbe84c2aefb0ac3cd16977f007d40c22133ca0b
Demkovychkate <Panchevakate1804@gmail.com>
2024-02-17 23:13:35 +0200

    Change in base modal and card component

--- src/components/BaseModal/BaseModal.jsx
+++ src/components/BaseModal/BaseModal.jsx
@@ -2,65 +2,63 @@
 // import Card from '@mui/material/Card';
 // import CardContent from '@mui/material/CardContent';
 // import CardMedia from '@mui/material/CardMedia';
-// import Typography from '@mui/material/Typography';
 // import { CardActionArea } from '@mui/material';
+// import { Typography } from './styled';
 
-// const BaseModal = ({ title, image, description }) => (
-//     <Card sx={{ maxWidth: 345 }}>
+// const BaseModal = ({ image, description }) => (
+//   <div style={{
+//     display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',
+//   }}>
+//     <Card sx={{ maxWidth: 500 }}>
 //       <CardActionArea>
 //         <CardMedia
 //           component='img'
-//           height='300'
-//           width='300'
+//           height="350"
+//           width='100%'
+//           objectFit='cover'
 //           image={image}
 //         />
 //         <CardContent>
-//           <Typography gutterBottom variant='h5' component='div'>
-//             {title}
-//           </Typography>
-//           <Typography variant='body2' color='text.secondary'>
+//           <Typography variant="body3" color="text.primary">
 //             {description}
 //           </Typography>
 //         </CardContent>
 //       </CardActionArea>
 //     </Card>
+//   </div>
 // );
 
 // export default BaseModal;
 
-'use client';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+import { Typography } from './styled';
 
-import Image from 'next/image';
-import React from 'react';
-import { CardBody, CardContainer, CardItem } from '../ui/3d-card.tsx';
-
-const BaseModal = ({ title, image, description }) => ( 
-    <CardContainer className='inter-var'>
-      <CardBody className='bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  '>
-        <CardItem
-          translateZ='50'
-          className='text-xl font-bold text-neutral-600 dark:text-white'
-        >
-          {title}
-        </CardItem>
-        <CardItem
-          as='p'
-          translateZ='60'
-          className='text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300'
-        >
-          {description}
-        </CardItem>
-        <CardItem translateZ='100' className='w-full mt-4'>
-          <Image
-            src={image}
-            height='1000'
-            width='1000'
-            className='h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl'
-            alt='thumbnail'
-          />
-        </CardItem>    
-      </CardBody>
-    </CardContainer>
-  );
+const BaseModal = ({ image, description, handleClose }) => (
+  <div style={{
+    display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',
+  }}>
+    <Card sx={{ maxWidth: 500 }}>
+      <CardActionArea>
+        <CardMedia
+          component='img'
+          height="350"
+          width='100%'
+          objectFit='cover'
+          image={image}
+          onClick={handleClose} // Додайте обробник події onClick
+        />
+        <CardContent>
+          <Typography >
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  </div>
+);
 
 export default BaseModal;
---
+++ src/components/BaseModal/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Typography = styled.div`
+  font-family: "Protest Riot", sans-serif;
+  font-size: 20px;  
+  height: 300px;
+  overflow-y: auto;
+  padding: 0;
+`;
