4efaaec3c7ae032ee0aca7b514738cc39f15ced0
Demkovychkate <Panchevakate1804@gmail.com>
2024-02-18 02:49:28 +0200

    some change in SideBar.jsx

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,26 +1,15 @@
-// import React from 'react';
-// import { SideBarWrapper } from './styled';
-
-// const SideBar = () => (
-// <SideBarWrapper>SideBar</SideBarWrapper>
-// );
-
-// export default SideBar;
-
 import React, { useState } from 'react';
-import { SideBarWrapper, BurgerMenuIcon } from './styled'; // Додали BurgerMenuIcon
+import { SideBarWrapper, BurgerMenuIcon } from './styled';
 
 const SideBar = () => {
-  const [isOpen, setIsOpen] = useState(false); // Створили стан для відкриття/закриття сайдбару
-
-  // Функція для відкриття/закриття сайдбару
+  const [isOpen, setIsOpen] = useState(false);
   const toggleSideBar = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <SideBarWrapper isOpen={isOpen}> {/* Передали isOpen як пропс */}
-      <BurgerMenuIcon onClick={toggleSideBar}>&#9776;</BurgerMenuIcon> {/* Додали іконку бургер-меню, яка викликає функцію toggleSideBar */}
+    <SideBarWrapper isOpen={isOpen}>
+      <BurgerMenuIcon onClick={toggleSideBar}>&#9776;</BurgerMenuIcon>
       SideBar
     </SideBarWrapper>
   );
