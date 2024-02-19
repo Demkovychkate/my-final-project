38f3c7f92a87e433ae03eb6102ad9e7aaf07396f
Demkovychkate <Panchevakate1804@gmail.com>
2024-02-19 21:30:55 +0200

    Add NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Templates from './components/Templates/Templates';
+import NotFound from './pages/NotFound/Notfound';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Templates />} />
-      <Route path="/quiz/new" element={<div>New Quiz logic</div>} />
+      <Route path='/*' element= {<NotFound />} />
+      <Route path='/' element={<Templates />} />
+      <Route path='/quiz/new' element={<div>New Quiz logic</div>} />
       </Routes>
       </BrowserRouter>
   );
---
+++ src/pages/NotFound/Notfound.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NotFound = () => {
+<p>404</p>;
+};
+
+export default NotFound;
