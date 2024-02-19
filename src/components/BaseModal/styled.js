fa209b5da9f26a99f028157f2f8b19d5d074f499
Demkovychkate <Panchevakate1804@gmail.com>
2024-02-19 10:50:30 +0200

    Hidden scrollbars in SideBar component

--- src/components/BaseModal/styled.js
+++ src/components/BaseModal/styled.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const Typography = styled.div`
- font-family: "Indie Flower", cursive;
- font-weight: 600;
- font-style: normal;
+  font-family: "Indie Flower", cursive;
+  font-weight: 600;
+  font-style: normal;
   font-size: 25px;  
   height: 300px;
   overflow-y: auto;
@@ -11,4 +11,6 @@ export const Typography = styled.div`
   color: white;
   text-shadow: 2px 2px 4px rgba(0,0,0,0.5); 
   border-right: .15em solid black;
-  `;
+  &::-webkit-scrollbar {
+    display: none;
+  }`;
