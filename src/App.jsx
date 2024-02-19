b4bd2181d3cded270a2099602f93125acf57716f
Demkovychkate <Panchevakate1804@gmail.com>
2024-02-19 19:21:28 +0200

    Add React Router to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Templates from './components/Templates/Templates';
 
 function App() {
   return (
-    <Templates />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Templates />} />
+      <Route path="/quiz/new" element={<div>New Quiz logic</div>} />
+      </Routes>
+      </BrowserRouter>
   );
 }
 
